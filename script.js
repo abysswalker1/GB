18e72018e8c47ccfe362aa5c4fc1b1c673ad4c4c
Vladislav Filippov <vladislav.filippov.01@mail.ru>
2024-02-07 15:41:21 +0500

    mobile-navigation + Modal

--- script.js
+++ script.js
@@ -27,12 +27,69 @@ class RadioPagination {
   };
 };
 
+class Modal {
+  constructor(id) {
+    this.window = document.getElementById(id);
+    this.modal = this.window.querySelector('.modal');
+    this.closeBtn = this.modal.querySelector('.modal-close');
+  };
+
+  open() {
+    this.window.classList.add('opened');
+    this.window.addEventListener('click', (e) => this._handleClick(e));
+    this.closeBtn?.addEventListener('click', () => this.close());
+  };
+
+  close() {
+    this.window.removeEventListener('click', (e) => this._handleClick(e));
+    this.closeBtn?.removeEventListener('click', () => this.close());
+    this.window.classList.remove('opened');
+  };
+
+  _handleClick(e) {
+    if (e.target === this.window) {
+      this.close();
+    }
+  }
+}
+
+
 (function() {
   const settingsArticleInformation = document.getElementById('settings-article-information');
   new RadioPagination(settingsArticleInformation).init();
 
-  const surveyForm = document.getElementById('opinion-form');
-  surveyForm.addEventListener('submit', function(e) {
+  const modalButtons = document.querySelectorAll('[data-modal-id]');
+
+  modalButtons.forEach(btn => {
+    const id = btn.dataset.modalId;
+    const modal = new Modal(id);
+
+    btn.addEventListener('click', () => modal.open());
+  })
+
+  const opinionForm = document.getElementById('opinion-form');
+  opinionForm.addEventListener('submit', function(e) {
     e.preventDefault();
   });
-}()); 
+}());
+
+// const breakpoints = {
+//   max: () => document.body.clientWidth > 1440,
+//   medium: () => document.body.clientWidth <= 1440,
+//   small: () => document.body.clientWidth <= 768,
+// };
+
+// class NodeReplace {
+//   constructor(nodes) {
+//     this.element = nodes.element;
+//     this.firstPlace = nodes.firstPlace;
+//     this.secondPlace = nodes.secondPlace;
+//   }
+//   replace() {
+
+//   }
+// }
+
+// window.addEventListener('resize', function() {
+
+// });
